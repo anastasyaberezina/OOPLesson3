c5eaa401e7b6dbdf9d093c5df7d13f3c597d8073
Anastasiya B <gavrilovaanastasya@yandex.ru>
2023-03-07 17:07:49 +0300

    add relat-s

---
+++ Relations.java
@@ -0,0 +1,11 @@
+package Per;
+
+public class Relations { //Класс родственных связей, содержащий свойства класса
+    static Person purent;
+    static Person child;
+    static Person vife;
+    static Person husbent;
+    static Person brother;
+    static Person sister;
+}
+
