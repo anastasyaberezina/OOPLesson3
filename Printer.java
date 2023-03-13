ba8338098c38d782530172ea91ddd740a5e17c01
Anastasiya B <gavrilovaanastasya@yandex.ru>
2023-03-13 09:50:12 +0300

    add Printer

--- Printer.java
+++ Printer.java
@@ -1,2 +1,18 @@
-package Per;public class Printer {
+package Per;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public interface Printer {
+    void print() throws IOException;
+    default void print(ArrayList<Person> personArrayList) throws IOException { // Этот метод сохраняет данные в файл
+        FileWriter fileWriter = new FileWriter("Research.txt", true);
+        for (Person person: personArrayList) {  // берет ArrayList и записывает имена из него в файл
+            fileWriter.write(person.getName());
+            fileWriter.write('\n');
+        }
+        fileWriter.write("---");
+        fileWriter.write('\n');
+        fileWriter.flush;
+    }
 }
