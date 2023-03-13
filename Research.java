5dd5f2c38a2cba9a71bd2e2d720e6738f04a38ec
Anastasiya B <gavrilovaanastasya@yandex.ru>
2023-03-13 09:51:21 +0300

    add Research

--- Research.java
+++ Research.java
@@ -1,22 +1,10 @@
 package Per;
 
-import com.sun.source.tree.Tree;
+import java.io.IOException;
 import java.util.ArrayList;
-public class Search extends Tree { //Класс с методом поиска
-    public ArrayList<String> res;
-    public ArrayList<String> getRes(){
-        return res;
-    }
-    ArrayList<Relationships.RelatedRelationship> rel;
-    public Search(Relationships rel){
-        res = rel.getName();
-    }
-    public ArrayList<String> spend (Person one, Person second, Relationships o) {
-        for (Relationships.RelatedRelationship x : rel) {
-            if (x.second.name == one.name && x.o == o) {
-                res.add (x.second.name);
-            }
-        }
-        return res;
-    }
+// Интерфейс
+public interface Research {
+    ArrayList<Person> getParents(Relationships relationships) throws IOException; // Методы, требующие реализации
+    ArrayList<Person> getChildrenByPerson() throws IOException;
 }
+
