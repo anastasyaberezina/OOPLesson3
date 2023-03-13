e7054c97ad18d81eee628f7127a2373eab83eb31
Anastasiya B <gavrilovaanastasya@yandex.ru>
2023-03-13 09:49:22 +0300

    add Node

--- Node.java
+++ Node.java
@@ -1,2 +1,16 @@
-package Per;public class Node {
+package Per;
+
+public class Node {
+    Person person;
+    Relationships relationships;
+    Person anotherPerson;
+
+    public Node(Person person, Relationships relationships, Person anotherPerson){
+        this.person = person;
+        this.relationships = relationships;
+        this.anotherPerson = anotherPerson;
+    }
+
+    @Override
+    public String toString() { return String.format("<%s %s %s>", person, relationships, anotherPerson);}
 }
