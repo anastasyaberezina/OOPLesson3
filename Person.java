2748c4f6d2a8e4f971c15e77da7dfd0a5b217733
Anastasiya B <gavrilovaanastasya@yandex.ru>
2023-03-13 09:49:42 +0300

    add Person

--- Person.java
+++ Person.java
@@ -1,37 +1,56 @@
 package Per;
 
-public class Person { // Класс и свойства класса
-    public String name;
-    public int age;
+import lombok.Getter; //?
+import lombok.Setter; //?
+import java.io.IOException;
+import java.util.ArrayList;
+
+@Getter
+@Setter
+public class Person implements GeoTree, Research, Printer{ // Класс и свойства класса
+    private String name;
+    private int age;
 
     public Person(String name, int age){ // Конструктор
-
         this.name = name;
         this.age = age;
     }
 
-    public String getName(){ // геттер для имени
+    @Override
+    public String toString() { return name; }
 
-        return name;
-    }
-
-    public void setName(String Name){ // сеттер для имени
-
-        this.name=name;
-    }
-
-    public int getAge() { // геттер для возраста
-
-        return age;
-    }
-
-    public void setAge(int age){ // сеттер для возраста
-
-        this.age=age;
+    @Override
+    public void append(Person children) {
+        getTree().add. (new Node(this, Relationships.PARENT, children));
+        getTree().add. (new Node(children, Relationships.CHILDREN, this));
     }
 
     @Override
-    public String toString(){
-        return "Person{" + "name:" + name + "}";
+    public ArrayList<Person> getParents(Relationships relationships) throws IOException {
+        ArrayList<Person> result = new ArrayList<>();
+        for (Node node : getTree()) {
+            if (node.person.getName().equals(this.getName()) && node.relationships.equals(relationships)) {
+                result.add(node.anotherPerson);
+            }
+        }
+        print(result);
+        return result;
+    }
+
+    @Override
+    public ArrayList<Person> getChildrenByPerson() throws IOException {
+        ArrayList<Person> result = new ArrayList<>();
+        for (Node node: getTree()) {
+            if (node.person.getName().equals(this.getName())
+                && node.relationships.equels(Relationships.PARENT)) {
+                result.add(node.anotherPerson);
+            }
+        }
+        print(result);
+        return result;
     }
 }
+
+
+
+
